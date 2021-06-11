67524a2b0d5ce12c45b632f33920a43287764737
吴东 <511970747@qq.com>
2021-06-11 22:50:15 +0800

    typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,22 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-// import { UsersService } from './users/users.service';
-// import { UsersModule } from './users/users.module';
 import { AuthService } from './auth/auth.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    TypeOrmModule.forRoot(),
     AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService, AuthService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
