eb8ea67633e93b572e3e346aeeaa763548ccaa5f
吴东 <511970747@qq.com>
2022-01-08 12:22:25 +0800

    feat: 更新sh脚本

--- server.sh
+++ server.sh
@@ -2,7 +2,9 @@
 
 echo '---开始执行git pull---'
 git pull
-echo '---git pull执行完毕，开始执行yarn build---'
+echo '---git pull执行完毕，开始执行yarn install---'
+yarn
+echo '---yarn install执行完毕，开始执行yarn build---'
 yarn build
 echo '---yarn build执行完毕 开始执行yarn pm2---'
 yarn pm2
