14d6d8dbcc3d0a49e120c682674cc16414ffac29
吴东 <511970747@qq.com>
2022-01-20 15:05:37 +0800

    chore: 完善环境变量

--- server-prod.sh
+++ server-prod.sh
@@ -6,8 +6,10 @@ echo '---开始执行git pull---'
 git pull
 echo '---git pull执行完毕，开始执行yarn install---'
 yarn
-echo '---yarn install执行完毕，开始执行yarn build:prod---'
-yarn build:prod
-echo '---yarn build执行完毕 开始执行yarn pm2---'
-yarn pm2
+echo '---yarn install执行完毕，开始执行yarn build---'
+yarn build
+echo '---yarn build执行完毕 开始设置环境变量---'
+export NODE_ENV=prod
+echo '---环境变量设置完毕 开始执行yarn pm2---'
+yarn pm2:prod
 echo '---yarn pm2执行完毕 服务启动成功---'
--- server-test.sh
+++ server-test.sh
@@ -6,8 +6,10 @@ echo '---开始执行git pull---'
 git pull
 echo '---git pull执行完毕，开始执行yarn install---'
 yarn
-echo '---yarn install执行完毕，开始执行yarn build:test---'
-yarn build:test
-echo '---yarn build执行完毕 开始执行yarn pm2---'
-yarn pm2
+echo '---yarn install执行完毕，开始执行yarn build---'
+yarn build
+echo '---yarn build执行完毕 开始设置环境变量---'
+export NODE_ENV=test
+echo '---环境变量设置完毕 开始执行yarn pm2---'
+yarn pm2:test
 echo '---yarn pm2执行完毕 服务启动成功---'
