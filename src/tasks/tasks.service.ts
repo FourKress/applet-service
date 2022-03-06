8d3480bcd6f9631bc5f6905435eb7d898b45809a
吴东 <511970747@qq.com>
2022-03-06 13:43:10 +0800

    feat: 自动发送-测试2

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -36,7 +36,7 @@ export class TasksService {
     await this.wxService.updateCertificates();
   }
 
-  @Cron('0 0 13 * * *')
+  @Cron('0 50 14 * * *')
   async handleStadiumAutoShare() {
     const wxGroupList = await this.wxGroupService.findActiveList();
     const stadiumIds = wxGroupList.map((wxGroup) => wxGroup.stadiumId);
