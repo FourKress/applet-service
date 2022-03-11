c69d3a1339e3e372a2bcaa202725126dfbd69e37
吴东 <511970747@qq.com>
2022-03-11 14:17:21 +0800

    fix: 修改场主查询场次列表时的提示语

--- src/match/dto/match-run.dto.ts
+++ src/match/dto/match-run.dto.ts
@@ -1,6 +1,7 @@
 import { IsString, IsMongoId, IsOptional, IsNotEmpty } from 'class-validator';
 
 export class MatchRunDto {
+  @IsOptional()
   @IsMongoId({ message: 'stadiumId 不是正确的ID类型' })
   readonly stadiumId: string;
 
