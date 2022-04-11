4ed589ed6810807d325819873119e7e85196f7ff
吴东 <511970747@qq.com>
2022-04-11 13:31:58 +0800

    feat: 提现功能1

--- src/withdraw/withdraw.service.ts
+++ src/withdraw/withdraw.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -7,9 +7,11 @@ import { Withdraw, WithdrawDocument } from './schemas/withdraw.schema';
 import { WxService } from '../wx/wx.service';
 import { UsersService } from '../users/users.service';
 import { UserEntity } from '../auth/interfaces/user-entity.interface';
+import { ToolsService } from '../common/utils/tools-service';
 
 @Injectable()
 export class WithdrawService {
+  private readonly logger = new Logger(WithdrawService.name);
   constructor(
     @InjectModel(Withdraw.name)
     private readonly withdrawModel: Model<WithdrawDocument>,
@@ -28,16 +30,23 @@ export class WithdrawService {
       withdrawId,
     });
     const { status, ...result } = wxResult;
+    this.logger.log(result);
     await this.withdrawModel.findByIdAndUpdate(withdrawId, {
       ...wxResult,
     });
     if (status) {
       const userFormDB = await this.usersService.findByBossId(bossId);
       const { balanceAmt } = userFormDB;
-      console.log(balanceAmt);
+
+      this.logger.log(`${balanceAmt}, ${amount}`);
+      await this.usersService.modify({
+        id: userId,
+        balanceAmt: balanceAmt - amount,
+      });
+
+      return true;
     } else {
-      console.log(result);
+      return ToolsService.fail('微信提现失败');
     }
-    return wxResult;
   }
 }
