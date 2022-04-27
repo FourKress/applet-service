18bcd481de89a31ee6a2a5168676c435fb4ccc1b
吴东 <511970747@qq.com>
2022-04-27 10:51:56 +0800

    fix: 我的顾客月卡状态bug修复

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -707,6 +707,7 @@ export class OrderService {
     );
     return {
       ...order.user,
+      orderCreatedAt: order.createdAt,
       count: filterList.length,
       monthlyCardCount: filterList.filter((d) => d.isMonthlyCard).length,
       lastTime: filterList[0].createdAt,
@@ -754,7 +755,7 @@ export class OrderService {
             userId: user.id,
             stadiumId: user.stadiumId,
           },
-          user.createdAt,
+          user.orderCreatedAt,
         );
         return {
           ...user,
